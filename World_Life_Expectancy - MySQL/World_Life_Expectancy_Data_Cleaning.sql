1c2e87b1c5ccb6bd0f21f471ca9a5d1fb82097a1
Amit <112909022+Murse-Project@users.noreply.github.com>
2024-04-22 22:27:06 +0300

    Update World_Life_Expectancy_Data_Cleaning.sql

--- World_Life_Expectancy - MySQL/World_Life_Expectancy_Data_Cleaning.sql
+++ World_Life_Expectancy - MySQL/World_Life_Expectancy_Data_Cleaning.sql
@@ -3,13 +3,15 @@ FROM worldlifexpectancy
 LIMIT 5000;
 
 -- Trying to find if there are any dupes in the data set
--- Later on I can use this query to make sure everything is cleaned.
+-- Later on I can use this query to make sure everything is cleaned
 SELECT Country, Year, CONCAT(Country, Year), COUNT(CONCAT(Country, Year))
 FROM worldlifexpectancy
 GROUP BY Country, Year, CONCAT(Country, Year)
-HAVING COUNT(CONCAT(Country, Year)) > 1
-;
+HAVING COUNT(CONCAT(Country, Year)) > 1;
+
+
 -- There are dupes, now I need to remove them
+-- Finding the dupes row numbers, a query I will later use to update the dataset
 SELECT *
 FROM (
 	SELECT Row_ID, CONCAT(Country, Year),
@@ -17,7 +19,8 @@ FROM (
 	FROM worldlifexpectancy
 ) AS Row_Table
 WHERE Row_NUM > 1;
--- Delete query, removing the found dupes
+
+-- After making sure everything works it's time to update the dataset
 DELETE FROM worldlifexpectancy
 WHERE
 	Row_ID IN (
@@ -29,10 +32,10 @@ WHERE
 		) AS Row_Table
 		WHERE Row_NUM > 1
         );
--- 3 Rows were affected as planned. 
+-- 3 Rows were affected after running the query as planned. 
 
--- How many rows are missing data in the status column 
--- Could serve as a test as well after cleaning the data, making sure it's empty
+-- Now after running over the dataset I could see that the 'Status' column is missing few values
+-- This query could serve as a test as well after cleaning the data
 SELECT *
 FROM worldlifexpectancy
 WHERE Status = '';
@@ -59,14 +62,15 @@ AND table_two.Status <> ''
 AND table_two.Status = 'Developed';
 -- 1 Row was affected
 
--- The next column needed cleaning is the life expectancy one
--- We can use this query as a test after updating the data to see it its empty to make sure we cleaned everthing
+-- The next column that is missing some values is the life expectancy one
+-- We can use this query as a test after updating the data
 SELECT Country, Year, Lifeexpectancy
 FROM worldlifexpectancy
 WHERE Lifeexpectancy = '';
 
+-- Few things we can see while analyzing the life expectancy column 
 -- Life expectancy is mostly increasing each year and is steady. 
--- We can average the year above and below the year with missing data
+-- We can average the year above and below the year with missing data to get a rough estimate of the missing values
 -- If 2018 data is missing we will average 2019 with 2017 
 SELECT table_one.Country, table_one.Year, table_one.Lifeexpectancy,
 		table_two.Country, table_two.Year, table_two.Lifeexpectancy,
@@ -91,3 +95,5 @@ JOIN worldlifexpectancy table_three
 SET table_one.Lifeexpectancy =  ROUND((table_two.Lifeexpectancy + table_three.Lifeexpectancy) / 2, 1)
 WHERE table_one.Lifeexpectancy = '';
 -- 2 Rows updated
+
+
